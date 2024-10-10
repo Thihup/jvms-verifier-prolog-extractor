9b7f5c40e9ab7447d702e0a3e27165915dd52956
Thiago Henrique Hüpner <thihup@gmail.com>
2024-10-10 09:18:48 -0300

    Add JSpecify

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,8 @@
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
 open module dev.thihup.jvms.verifier.prolog.extractor {
     requires org.jsoup;
     requires dev.nipafx.args;
+    requires org.jspecify;
 }
