17e7f20b36c833ab03c5f8d7e3f9756cc7bfe10b
Thiago Henrique Hüpner <thihup@gmail.com>
2024-10-10 09:20:21 -0300

    Simplify extraction

--- src/main/java/dev/thihup/jvms/verifier/prolog/extractor/Main.java
+++ src/main/java/dev/thihup/jvms/verifier/prolog/extractor/Main.java
@@ -6,111 +6,97 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Parser;
 import org.jsoup.select.Elements;
+import org.jspecify.annotations.Nullable;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.Collections;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.StructuredTaskScope;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Gatherer;
+import java.util.stream.Gatherers;
 import java.util.stream.IntStream;
-
-import static java.util.concurrent.StructuredTaskScope.Subtask.State.SUCCESS;
 
 public class Main {
 
-    record Result(int version, String spec){}
+    record Result(int version, @Nullable String spec) {
+    }
 
-    public record Arguments(Optional<Integer> startVersion, Optional<Integer> endVersion, Optional<String> url, Optional<String> outputFolder, Optional<Boolean> keepDuplicates) {}
+    public record Arguments(Optional<Integer> startVersion, Optional<Integer> endVersion, Optional<String> url,
+                            Optional<String> outputFolder, Optional<Boolean> keepDuplicates, Optional<Boolean> fixProlog) {
+    }
 
     public static void main(String[] args) throws Throwable {
 
         Arguments parse = Args.parse(args, Arguments.class);
-        try(var scope = new StructuredTaskScope<Result>()) {
+        boolean keepDuplicates = parse.keepDuplicates().orElse(false);
+        boolean fixProlog = parse.fixProlog.orElse(true);
 
-            // Fetch the HTML content from the URL
-            String url = parse.url().orElse("https://docs.oracle.com/javase/specs/jvms/se%s/html/jvms-4.html#jvms-4.10");
-            List<StructuredTaskScope.Subtask<Result>> list = IntStream.rangeClosed(parse.startVersion().orElse(7), parse.endVersion().orElse(Runtime.version().feature()))
-                    .mapToObj(args1 -> Map.entry(args1, url.formatted(args1)))
-                    .map(map -> scope.fork(() -> new Result(map.getKey(), extracted(map.getValue())))).toList();
+        int start = parse.startVersion().orElse(7);
+        int end = parse.endVersion().orElse(Runtime.version().feature());
 
-            scope.join();
-
-            boolean b = parse.keepDuplicates().orElse(false);
-            Map<Integer, String> collect = list.stream()
-                .filter(x -> x.state() == SUCCESS)
-                .map(StructuredTaskScope.Subtask::get)
-                .gather(deduplicateAdjacent(Result::spec, b))
+        // Fetch the HTML content from the URL
+        String url = parse.url().orElse("https://docs.oracle.com/javase/specs/jvms/se%s/html/jvms-4.html#jvms-4.10");
+        Map<Integer, String> collect = IntStream.rangeClosed(start, end)
+                .mapToObj(args1 -> Map.entry(args1, url.formatted(args1)))
+                .gather(Gatherers.mapConcurrent(end - start, map -> new Result(map.getKey(), extracted(map.getValue(), fixProlog))))
+                .filter(x -> x.spec() != null)
+                .filter(distinctBy(Result::spec, keepDuplicates))
                 .collect(Collectors.toMap(Result::version, Result::spec));
 
-            for (Map.Entry<Integer, String> integerStringEntry : collect.entrySet()) {
-                Files.writeString(Paths.get(parse.outputFolder().orElse("."), "jvms-%s-prolog.pl".formatted(integerStringEntry.getKey())), integerStringEntry.getValue());
-            }
+        for (Map.Entry<Integer, String> integerStringEntry : collect.entrySet()) {
+            Files.writeString(Paths.get(parse.outputFolder().orElse("."), "jvms-%s-prolog.pl".formatted(integerStringEntry.getKey())), integerStringEntry.getValue());
         }
 
     }
 
-    public static <T, R> Gatherer<T,?,T> deduplicateAdjacent(Function<T, R> mapper, boolean skip) {
-        class State { R prev; boolean hasPrev; }
-        return Gatherer.ofSequential(
-                State::new,
-                (state, element, downstream) -> {
-                    if (skip) {
-                        return downstream.push(element);
-                    }
-                    R apply = mapper.apply(element);
-                    if (!state.hasPrev) {
-                        state.hasPrev = true;
-                        state.prev = apply;
-                        return downstream.push(element);
-                    } else if (!Objects.equals(state.prev, apply)) {
-                        state.prev = apply;
-                        return downstream.push(element);
-                    } else {
-                        return true; // skip duplicate
-                    }
-                }
-        );
+    private static <T, R> Predicate<R> distinctBy(Function<R, T> a, boolean keepDuplicates) {
+        Set<T> objects = Collections.newSetFromMap(new ConcurrentHashMap<>());
+        return e -> keepDuplicates || objects.add(a.apply(e));
     }
 
-    private static String extracted(String url) {
-        Document doc = null;
+    @Nullable
+    private static String extracted(String url, boolean fixProlog) {
+        Document doc;
         try {
             doc = Jsoup.connect(url).get();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.err.println("Could not fetch " + url);
+            return null;
         }
 
         // Parse the HTML and find the div element containing the a tag with name='jvms-4.10'
         Element jvm410Node = doc.selectFirst("div.section:has(a[name=jvms-4.10])");
 
         // If the div is found, select all <pre class="programlisting"> elements inside it
-        if (jvm410Node != null) {
-            Elements preElements = jvm410Node.select("pre.programlisting");
-            // Print the outer HTML of the found elements
-            int size = preElements.size();
-            String prolog = preElements.stream()
-                    .limit(size - 3)
-                    .map(Element::text)
-                    .map(Object::toString)
-                    .filter(x -> !x.contains("Verification type hierarchy:"))
-                    .map(x -> Parser.unescapeEntities(x, false))
-                    .map(Main::fixProlog)
-                    .collect(Collectors.joining("\n"));
-
-            return prolog;
-
+        if (jvm410Node == null) {
+            System.err.println("Could not parse the return from " + url);
+            return null;
         }
-        throw new RuntimeException("Could not parse");
+        Elements preElements = jvm410Node.select("pre.programlisting");
+        // Print the outer HTML of the found elements
+        int size = preElements.size();
+
+        return preElements.stream()
+                .limit(size - 3)
+                .map(Element::text)
+                .map(Object::toString)
+                .filter(x -> !x.contains("Verification type hierarchy:"))
+                .map(x -> Parser.unescapeEntities(x, false))
+                .map((String s) -> fixProlog(s, fixProlog))
+                .collect(Collectors.joining("\n"));
+
     }
 
-    private static String fixProlog(String s) {
-
+    private static String fixProlog(String s, boolean fixProlog) {
+        if (!fixProlog) {
+            return s;
+        }
         // 4.10.1.8. Type Checking for protected Members
         // Current:
         //
